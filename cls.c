761d43cbe385dc2e2bf71e5d757ae316e97765e1
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2025-03-22 11:51:43 -0400

    Update cls.c

--- cls.c
+++ cls.c
@@ -1,10 +1,30 @@
 // This calls the library required for clearing the screen.
-#include <unistd.h>
+#include <stdio.h>
 
-// Defines what clear does specifically.
-#define CLEAR "clear"
+// Creates a function that clear the screen using external shell scripting.
+void external_clear() {
+  printf("Not yet available."); 
+}
 
-// Creates a function that allows you to clear the screen.
-void clear_screen() {
-  system(CLEAR);
+// Creates a function that allows you to clear the screen using ansi control sequences.
+void ansi_clear() {
+
+  // \e is the escape character (ASCII code 27)
+  // [ begins the ANSI control sequence
+  // 1;1 specifies row 1, column 1
+  // H indicates cursor movement
+
+  // \e[ starts another ANSI control sequence
+  // 2J clears the screen
+
+  // These two commands do not need to be merged. Do not merge them.
+  printf("\e[1;1H \e[2J");
+}
+
+int main() {
+  ansi_clear();
+
+  //printf("Hello World\n");
+
+  return 0;
 }
